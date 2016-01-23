858bf1137f7d135fd1a5a3e467674fa61c79e137
qnnnnez <qnnnnez@live.com>
2016-01-23 23:08:55 +0800

    增加转换脚本

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## 准备工作:
 * [Bitmap Font Generator](http://www.angelcode.com/products/bmfont/): 用于生成字库
 * [scpak](https://github.com/qnnnez/scpak): 用于打包字库
-* 一个脚本(暂未提供)，用于将 Bitmap Font Generator 生成的字体信息文件(font-name.fnt)转换为 scpak 能识别的格式
+* [python3](https://www.python.org/downloads/): 用于执行转换脚本
 
 ## 工作步骤:
 1. 使用 scpak 解包 Content.pak 为目录 Content
@@ -13,10 +13,9 @@
 1. 选择需要的汉字(可以从文件选择，目前 chars.txt 中包含了3500个常用汉字、字母、数字、特殊符号)
 1. 调整输出选项，使字体能保存在一张纹理当中，并尽可能小(注意，SC 不能读取超过长或宽超过 2048 像素的纹理)
 1. 渲染字体，保存为 font-name (此时应生成两个文件: font-name_0.tga, font-name.fnt)
-1. 使用脚本转换 font-name.fnt，生成 font-name.lst
+1. 使用 fnt2lst.py 转换 font-name.fnt，生成 font-name.lst，命令为: ```$ python3 fnt2lst.py font-name```
 1. 将 font-name_0.tga 重命名为font-name.tga, 与 font-name.lst 一同复制到解包的 Content 目录中
 1. 使用 scpak 打包 Content 目录为 Content.pak
 
 ## TODO List:
 * 待文本翻译完成，更新 chars.txt，使字库刚好够用
-* 更新用于转换的脚本
---
+++ fnt2lst.py
@@ -0,0 +1,74 @@
+class Line(dict):
+    def __init__(self, line):
+        super().__init__(self)
+        self.parse(line)
+        
+    def parse(self, line):
+        line = line.strip()
+        comps = line.split(' ')
+        if '=' not in comps[0]:
+            self.name = comps[0]
+            del comps[0]
+        else:
+            self.name = None
+        for comp in comps:
+            comp = comp.strip()
+            if not comp:
+                continue
+            key, value = comp.split('=')
+            self[key] = value
+
+def fnt2lst(fnt_path, lst_path, scale=1, fallback=95):
+    lst_file = open(lst_path, 'w')
+    lines = open(fnt_path, 'r', encoding='utf-8').readlines()
+
+    infos = Line(lines[0])
+    spacing = infos['spacing'].split(',')
+
+    commons = Line(lines[1])
+    line_height = int(commons['lineHeight'])
+    base = int(commons['base'])
+    width = int(commons['scaleW'])
+    height = int(commons['scaleH'])
+
+    char_count = int(Line(lines[3])['count'])
+    lst_file.write('{}\n'.format(char_count))
+
+    for i in range(4, char_count + 4):
+        line = lines[i]
+        infos = Line(line)
+        unicode = int(infos['id'])
+        x = int(infos['x'])
+        y = int(infos['y'])
+        w = int(infos['width'])
+        h = int(infos['height'])
+        xoffset = int(infos['xoffset'])
+        yoffset = int(infos['yoffset'])
+        xadvance = int(infos['xadvance'])
+    
+        coord1 = (x/width, y/height)
+        coord2 = ((x+w)/width, (y+h)/height)
+        offset = (xoffset, yoffset)
+        gwidth = xadvance
+    
+        lst_file.write(
+            '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(
+                unicode,
+                coord1[0], coord1[1],
+                coord2[0], coord2[1],
+                offset[0], offset[1] + line_height - base,
+                gwidth))
+
+        print(infos)
+
+    lst_file.write('{}\n'.format(line_height))
+    lst_file.write('{} {}\n'.format(spacing[0], spacing[1]))
+    lst_file.write('{}\n'.format(scale))
+    lst_file.write('{}\n'.format(fallback))
+    
+    lst_file.close()
+
+if __name__ == '__main__':
+    import sys
+    filename = sys.argv[1]
+    fnt2lst(filename + '.fnt', filename + '.lst')
