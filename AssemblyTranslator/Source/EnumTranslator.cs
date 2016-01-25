420ef0dc525a65df8eccc56fa9d508a1f7ec0246
qnnnnez <qnnnnez@live.com>
2016-01-25 22:59:26 +0800

    add EnumTranslator

---
+++ AssemblyTranslator/Source/EnumTranslator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace AssemblyTranslator
+{
+    /// <summary>
+    /// Translate enum types by replacing ToString method.
+    /// </summary>
+    public static class EnumTranslator
+    {
+        public static void TranslateEnum(TypeDefinition enumType, AssemblyDefinition assembly, Dictionary<string, string> translation)
+        {
+            // map EnumType to string
+            var translationMap = new Dictionary<int, string>();
+            foreach (var field in enumType.Fields)
+            {
+                if (field.IsDefinition && field.IsLiteral && field.IsStatic)
+                {
+                    // is a enum value definition
+                    int enumValue = (int)field.Constant;
+                    var defaultName = field.Name;
+                    var translatedName = translation[defaultName];
+                    translationMap.Add(enumValue, translatedName);
+                }
+            }
+
+            // inject EnumType.GetTranslation()
+            var mscorlib = AssemblyDefinition.ReadAssembly("mscorlib.dll");
+            var inspector = new TypeInspector();
+            inspector.ScanAssembly(mscorlib);
+            inspector.ScanAssembly(assembly);
+
+            var methodGetTranslationName = "GetTranslation" + enumType.Name;
+            var methodGetTranslation = new MethodDefinition(methodGetTranslationName, MethodAttributes.Public | MethodAttributes.Static, inspector.FindType("System.String"));
+            methodGetTranslation.Parameters.Add(new ParameterDefinition(enumType));
+            var il = methodGetTranslation.Body.GetILProcessor();
+
+            var ldarg_0s = new List<Instruction>();
+            var bne_uns = new List<Instruction>();
+            foreach (var pair in translationMap)
+            {
+                var enumValue = pair.Key;
+                var translatedName = pair.Value;
+
+                var ldarg_0 = il.Create(OpCodes.Ldarg_0);
+                ldarg_0s.Add(ldarg_0);
+                il.Append(ldarg_0);
+
+                il.Emit(OpCodes.Ldc_I4, enumValue);
+
+                var bne_un = il.Create(OpCodes.Bne_Un, ldarg_0);
+                bne_uns.Add(bne_un);
+                il.Append(bne_un);
+
+                il.Emit(OpCodes.Ldstr, translatedName);
+                il.Emit(OpCodes.Ret);
+            }
+            for (int i = 0; i < translationMap.Count - 1; ++i)
+            {
+                bne_uns[i].Operand = ldarg_0s[i + 1];
+            }
+            var end = il.Create(OpCodes.Nop);
+            il.Append(end);
+            il.Emit(OpCodes.Ldstr, "error from qnnnnez: no match found");
+            il.Emit(OpCodes.Ret);
+            var last = bne_uns.Last();
+            last.Operand = methodGetTranslation.Body.Instructions.Last().Previous; // ldstr
+            
+            inspector.FindType("Game.Program").Methods.Add(methodGetTranslation);
+
+            // replace EnumType.ToString() with EnumType.GetTranslation()
+            var walker = new TypeWalker();
+            walker.MethodFound += delegate (TypeDefinition type, MethodDefinition method)
+            {
+                if (!method.HasBody)
+                    return;
+                foreach (var instruction in method.Body.Instructions)
+                {
+                    if (instruction.OpCode != OpCodes.Box)
+                        continue;
+                    if (instruction.Operand != enumType)
+                        continue;
+                    instruction.OpCode = OpCodes.Nop;
+                    instruction.Operand = null;
+                    var nextInstruction = instruction.Next;
+                    if (nextInstruction.OpCode != OpCodes.Callvirt)
+                        continue;
+                    var calledMethod = nextInstruction.Operand as MethodReference;
+                    if (calledMethod.ToString() == "System.String System.Object::ToString()")
+                    {
+                        nextInstruction.OpCode = OpCodes.Call;
+                        nextInstruction.Operand = inspector.FindType("Game.Program").Methods.Single(m => m.Name == methodGetTranslationName) as System.Object;
+                    }
+                }
+            };
+            walker.WalkAssembly(assembly);
+        }
+
+    }
+}
