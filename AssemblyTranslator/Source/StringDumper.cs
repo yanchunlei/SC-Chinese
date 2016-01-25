e34438d236f105523339f8113386626bb79bcd74
qnnnnez <qnnnnez@live.com>
2016-01-25 20:41:47 +0800

    move source files into Source/

---
+++ AssemblyTranslator/Source/StringDumper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace AssemblyTranslator
+{
+    /// <summary>
+    /// Dump all strings and type, method infos in an assembly.
+    /// </summary>
+    static class StringDumper
+    {
+        public static void DumpStrings(AssemblyDefinition assembly)
+        {
+            var walker = new TypeWalker();
+            walker.MethodFound += MethodFound;
+            walker.WalkAssembly(assembly);
+        }
+
+        private static void MethodFound(TypeDefinition type, MethodDefinition method)
+        {
+            if (!method.HasBody)
+                return;
+            foreach(var instruction in method.Body.Instructions)
+            {
+                if (instruction.OpCode == OpCodes.Ldstr)
+                {
+                    string s = instruction.Operand as string;
+                    if (s.Contains('{') || s.Contains('\\') || s.Contains('_'))
+                        continue;
+                    if (s == "\n")
+                        continue;
+                    if (s.Trim() == "")
+                        continue;
+                    string output = String.Format("{0}/{1}:{2}", type.FullName, method.Name,s);
+                    Console.WriteLine(output);
+                }
+            }
+        }
+    }
+}
---
+++ AssemblyTranslator/Source/TypeWalker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mono.Cecil;
+
+namespace AssemblyTranslator
+{
+    /// <summary>
+    /// Walk through a assembly. Report all types, fields, and methods.
+    /// </summary>
+    class TypeWalker
+    {
+        public event Action<TypeDefinition> TypeFound;
+        public event Action<TypeDefinition, FieldDefinition> FieldFound;
+        public event Action<TypeDefinition, MethodDefinition> MethodFound;
+
+        public TypeWalker()
+        {
+            TypeFound += delegate (TypeDefinition t) { };
+            FieldFound += delegate (TypeDefinition t, FieldDefinition f) { };
+            MethodFound += delegate (TypeDefinition t, MethodDefinition m) { };
+        }
+
+        public void WalkAssembly(AssemblyDefinition asembly)
+        {
+            foreach (var module in asembly.Modules)
+            {
+                WalkModule(module);
+            }
+        }
+
+        public void WalkModule(ModuleDefinition module)
+        {
+            foreach (var type in module.Types)
+            {
+                TypeFound(type);
+                WalkType(type);
+            }
+        }
+
+        public void WalkType(TypeDefinition type)
+        {
+            foreach (var field in type.Fields)
+            {
+                FieldFound(type, field);
+            }
+            foreach (var method in type.Methods)
+            {
+                MethodFound(type, method);
+            }
+            foreach (var nestedType in type.NestedTypes)
+            {
+                TypeFound(nestedType);
+                WalkType(nestedType);
+            }
+        }
+    }
+}
