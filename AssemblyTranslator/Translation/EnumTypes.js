565427432b345f8f67167cecbb3e2526d8a5b224
qnnnnez <qnnnnez@live.com>
2016-01-26 16:37:09 +0800

    more enum translation

--- AssemblyTranslator/Translation/EnumTypes.js
+++ AssemblyTranslator/Translation/EnumTypes.js
@@ -1,8 +1,9 @@
 ﻿function translateEnum(fullName, translation) {
-    System.Console.WriteLine("Translating enum: " + fullName);
+    System.Console.Write("Translating enum: " + fullName + " ...");
     var AssemblyTranslator = importNamespace("AssemblyTranslator");
     translation = generateTranslation(translation);
     AssemblyTranslator.EnumTranslator.TranslateEnum(inspector.FindType(fullName), inspector, translation);
+    System.Console.WriteLine(" Done.");
 }
 
 function generateTranslation(object) {
@@ -28,4 +29,48 @@ translateEnum("Game.GameMode", {
     "Challenging": "挑战",
     "Cruel": "残酷",
     "Adventure": "冒险"
+});
+
+translateEnum("Game.EnvironmentBehaviorMode", {
+    "Living": "动态",
+    "Static": "静态"
+});
+
+translateEnum("Game.ExternalContentType", {
+    "Unknown": "未知",
+    "Directory": "目录",
+    "BlocksTexture": "方块纹理",
+    "CharacterSkin": "角色皮肤",
+    "World": "世界"
+});
+
+translateEnum("Game.LookControlMode", {
+    "Pad": "托盘",
+    "EntireScreen": "全屏幕",
+    "SplitTouch": "分离 触控"
+});
+
+translateEnum("Game.MoveControlMode", {
+    "Pad": "托盘",
+    "Buttons": "按钮"
+});
+
+translateEnum("Game.SkyRenderingMode", {
+    "Full": "完全",
+    "NoClouds": "无云",
+    "Disabled": "禁用"
+});
+
+translateEnum("Game.TimeOfDayMode", {
+    "Changing": "变化",
+    "Day": "白天",
+    "Night": "晚上",
+    "Sunrise": "日出",
+    "Sunset": "日落"
+});
+
+translateEnum("Game.ViewAngleMode", {
+    "Normal": "正常",
+    "Narrow": "窄",
+    "Wide": "宽"
 });
