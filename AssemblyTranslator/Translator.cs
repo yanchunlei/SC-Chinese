5766ae27d20ebb0f35ff919b493f8c1d6ce31be8
qnnnnez <qnnnnez@live.com>
2016-07-05 15:57:15 +0800

    adapt to monodroid

--- AssemblyTranslator/Translator.cs
+++ AssemblyTranslator/Translator.cs
@@ -16,68 +16,67 @@ namespace AssemblyTranslator
     {
         public static void TranslateEnum(TypeDefinition enumType, TypeInspector inspector, Dictionary<string, string> translation)
         {
-            // map EnumType to string
-            var translationMap = new Dictionary<int, string>();
-            foreach (var field in enumType.Fields)
-            {
-                if (field.IsDefinition && field.IsLiteral && field.IsStatic)
-                {
-                    // is a enum value definition
-                    int enumValue = (int)field.Constant;
-                    var defaultName = field.Name;
-                    var translatedName = translation[defaultName];
-                    translationMap.Add(enumValue, translatedName);
-                }
-            }
-
             // inject EnumType.GetTranslation()
             var methodGetTranslationName = "GetTranslation" + enumType.Name;
             var methodGetTranslation = new MethodDefinition(methodGetTranslationName, MethodAttributes.Public | MethodAttributes.Static, inspector.FindType("System.String"));
-            methodGetTranslation.Parameters.Add(new ParameterDefinition(enumType));
+            methodGetTranslation.Parameters.Add(new ParameterDefinition(inspector.FindType("System.String")));
             var il = methodGetTranslation.Body.GetILProcessor();
 
             var ldarg_0s = new List<Instruction>();
-            var bne_uns = new List<Instruction>();
-            foreach (var pair in translationMap)
+            var brfalses = new List<Instruction>();
+
+            var stringEqualsMethodExternal = inspector.FindType("System.String").Methods.Single(m => {
+                if (m.Name != "Equals")
+                    return false;
+                if (m.Parameters.Count != 2)
+                    return false;
+                if (m.Parameters[1].ParameterType.ToString() != "System.String")
+                    return false;
+                return true;
+            });
+            var stringEqualsMethod = enumType.Module.ImportReference(stringEqualsMethodExternal);
+            foreach (var pair in translation)
             {
-                var enumValue = pair.Key;
+                var originalName = pair.Key;
                 var translatedName = pair.Value;
 
                 var ldarg_0 = il.Create(OpCodes.Ldarg_0);
                 ldarg_0s.Add(ldarg_0);
                 il.Append(ldarg_0);
 
-                il.Emit(OpCodes.Ldc_I4, enumValue);
+                il.Emit(OpCodes.Ldstr, originalName);
+                il.Emit(OpCodes.Call, stringEqualsMethod);
 
-                var bne_un = il.Create(OpCodes.Bne_Un, ldarg_0);
-                bne_uns.Add(bne_un);
-                il.Append(bne_un);
+                var brfalse = il.Create(OpCodes.Brfalse, ldarg_0);
+                brfalses.Add(brfalse);
+                il.Append(brfalse);
 
                 il.Emit(OpCodes.Ldstr, translatedName);
                 il.Emit(OpCodes.Ret);
             }
-            for (int i = 0; i < translationMap.Count - 1; ++i)
+            for (int i = 0; i < translation.Count - 1; ++i)
             {
-                bne_uns[i].Operand = ldarg_0s[i + 1];
+                brfalses[i].Operand = ldarg_0s[i + 1];
             }
             var end = il.Create(OpCodes.Nop);
             il.Append(end);
             il.Emit(OpCodes.Ldstr, "error from qnnnnez: no match found");
             il.Emit(OpCodes.Ret);
-            var last = bne_uns.Last();
+            var last = brfalses.Last();
             last.Operand = methodGetTranslation.Body.Instructions.Last().Previous; // ldstr
             
             inspector.FindType("Game.Program").Methods.Add(methodGetTranslation);
 
             // replace EnumType.ToString() with EnumType.GetTranslation()
             var walker = new TypeWalker();
+            var patchList = new List<Action>();
             walker.MethodFound += delegate (TypeDefinition type, MethodDefinition method)
             {
                 if (!method.HasBody)
                     return;
                 foreach (var instruction in method.Body.Instructions)
                 {
-                    if (instruction.OpCode != OpCodes.Box)
+                    if ((instruction.OpCode != OpCodes.Box) && (instruction.OpCode != OpCodes.Constrained))
                         continue;
                     if (instruction.Operand != enumType)
                         continue;
@@ -85,17 +84,25 @@ namespace AssemblyTranslator
                     var nextInstruction = instruction.Next;
                     if (nextInstruction.OpCode != OpCodes.Callvirt)
                         continue;
-                    instruction.OpCode = OpCodes.Nop;
-                    instruction.Operand = null;
                     var calledMethod = nextInstruction.Operand as MethodReference;
                     if (calledMethod.ToString() != "System.String System.Object::ToString()")
                         continue;
-                    nextInstruction.OpCode = OpCodes.Call;
-                    nextInstruction.Operand = inspector.FindType("Game.Program").Methods.Single(m => m.Name == methodGetTranslationName) as System.Object;
+
+                    patchList.Add(() =>
+                    {
+                        var il2 = method.Body.GetILProcessor();
+                        var translateMethod = inspector.FindType("Game.Program").Methods.Single(m => m.Name == methodGetTranslationName);
+                        var translateInstruction = il.Create(OpCodes.Call, translateMethod);
+                        il2.InsertAfter(nextInstruction, translateInstruction);
+                    });
                 }
             };
             walker.WalkAssembly(enumType.Module.Assembly);
+
+            foreach (var patch in patchList)
+                patch();
         }
+
 
         public static int ReplaceStrings(MethodDefinition method, Dictionary<string, string> translation)
         {
